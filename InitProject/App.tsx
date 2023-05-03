b38a1368ee6dfe1a511e0ad4f00a3b0fcba53ebb
Rashad <87971037+RashadM0@users.noreply.github.com>
2023-05-03 18:52:15 +0400

    Init

---
+++ InitProject/App.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text >App</Text>
+      </View>
+
+
+
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+});
